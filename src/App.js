6f784575b51bc0252bc8c490d1e1088f7e85df47
“Pia <pia.hoelzl@gmx.at>
2024-01-26 11:02:33 +0100

    Change typo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import
+
 export default function AddingGuest() {
   //defining const variables
   const [inputFirstName, setInputFirstName] = useState('');
@@ -8,43 +10,46 @@ export default function AddingGuest() {
   const [lastName, setLastName] = useState('');
   const [isAttending, setIsAttending] = useState(false);
 
-  //handle event; save new user on key press enter
+  //function handle event; save new user on key press enter
   function handleSubmit(event) {
-    if (event.key === 'Enter')
-      const newUser = {
+    if (event.key === 'Enter') {
+      const newGuest = {
         firstName: inputFirstName,
         lastName: inputLastName,
-        isAttending: false
-      }
+        isAttending: false,
+        userID: userID,
+      };
     }
+  }
 
-      return (
-        <div>
-          <header>
-              <h1>Guest List</h1>
-            </header>
-            <form onSubmit={handleSubmit}>
-              <label>
-                First Name
-                <input
-                  placeholder="first name"
-                  onChange={(event) => {
-                    setFirstName(event.target.value);
-                  }}
-                />
-              </label>
-              <label>
-                Last Name
-                <input
-                  placeholder="last name"
-                  onChange={(event) => {
-                    setLastName(event.target.value);
-                  }}
-                />
-              </label>
-              <button>Submit</button>
-            </form>
-            <div>{/* {firstName} {lastName} */}</div>
-      </div>
-      );
-    }
+  return (
+    <div>
+      <header>
+        <h1>Guest List</h1>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            placeholder="Michael"
+            onChange={(event) => {
+              setFirstName(event.target.value);
+            }}
+          />
+        </label>
+
+        <label>
+          Last Name:
+          <input
+            placeholder="Scott"
+            onChange={(event) => {
+              setLastName(event.target.value);
+            }}
+          />
+        </label>
+        <button>Attend</button>
+      </form>
+      <div>{/* {firstName} {lastName} */}</div>
+    </div>
+  );
+}
