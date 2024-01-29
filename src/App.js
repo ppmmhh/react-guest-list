88cee0e2a3b501e37ef5e898efd859581396f406
“Pia <pia.hoelzl@gmx.at>
2024-01-29 20:12:10 +0100

    Add data-test-id

--- src/App.js
+++ src/App.js
@@ -56,49 +56,91 @@ export default function AddingGuest() {
   setLastName('');
 }
 
-return (
-  <>
-    <div>
-      <header>
-        <h1>Guest List</h1>
-      </header>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          handleClick();
-          setFirstName('');
-          setLastName('');
-        }}
-      >
-        <label>
-          First Name:
-          <input
-            placeholder="Enter First Name"
-            onChange={(event) => {
-              setFirstName(event.target.value);
-            }}
-          />
-        </label>
-        ,
-        <label>
-          Last Name:
-          <input
-            placeholder="Enter Last Name"
-            onChange={(event) => {
-              setLastName(event.target.value);
-            }}
-          />
-        </label>
-        ,<button>Enter</button>
-      </form>
-    </div>
+async function Guest(id) {
+  const response = await fetch(`${baseUrl}/${id}`, {
+    method: 'DELETE',
+  });
+  const deletedGuest = await response.json();
+  const newList = guests.filter((user) => user.id !== deletedGuest.id);
+  setGuests(newList);
+}
 
-    <ul>
-      {guests.map((guests) => (
-        <li key={guests.id}>
-          {`Guest ${guest.id}: ${guest.firstName} ${guest.lastName}`}
-        </li>
-      ))}
-    </ul>
-  </>
-);
+if (isLoading) {
+  return <div>Loading...</div>;
+} else {
+  return (
+    <div>
+      <h1> Guest List</h1>
+      <form>
+        <div>
+          <label>
+            First name:
+            <br />
+            <input
+              placeholder="Enter your first name"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Last name:
+            <br />
+            <input
+              placeholder="Enter last name"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  setFirstName('');
+                  setLastName('');
+                  newGuest().catch((error) => {
+                    console.log(error);
+                  });
+                }
+              }}
+            />
+          </label>
+        </div>
+      </form>
+      <div>
+        <h2>Registered Guests</h2>
+        {guests.map((user) => (
+          <div key={`user-${user.id}`} data-test-id="guest">
+            <div>
+              First Name: {user.firstName}
+              <br />
+              Last Name: {user.lastName}
+              <br />
+              Attendance: {JSON.stringify(user.attending)}
+              <br />
+              <label key={`user-${user.id}`}>
+                Attending:
+                <input
+                  type="checkbox"
+                  checked={user.attending}
+                  aria-label={`${user.firstName} ${user.lastName} attending status`}
+                  onChange={() => Guest(user.id, user.attending)}
+                />
+              </label>
+              <br />
+            </div>
+            <button
+              type="button"
+              aria-label={`Remove ${user.firstName} ${user.lastName}`}
+              onClick={() => {
+                cancelGuest(user.id).catch((error) => {
+                  console.log(error);
+                });
+              }}
+            >
+              Remove
+            </button>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
