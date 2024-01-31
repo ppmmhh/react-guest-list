53b1af9029dff7e5d3174207e7f40a73b4c2f813
“Pia <pia.hoelzl@gmx.at>
2024-01-31 20:36:51 +0100

    Fix curly braces in last line

--- src/App.js
+++ src/App.js
@@ -74,83 +74,83 @@ export default function AddingGuest() {
     return 'Loading...';
   }
   return (
-      <div className="guestlist">
-        <h1>Guest List</h1>
-        <form
+    <div className="guestlist">
+      <h1>Guest List</h1>
+      <form
         onSubmit={(event) => {
           event.preventDefault();
           createGuest().catch((error) => console.log(error));
           setFirstName('');
           setLastName('');
-        }}>
-          <div>
-            First name
-            <input
-              name="firstname"
-              placeholder="type first name"
-              value={firstName}
-              onChange={(event) => setFirstName(event.target.value)}
-            />
-          </div>
-        </form>
-
+        }}
+      >
         <div>
-          Last name
+          First name
           <input
-            name="lastname"
-            placeholder="type last name"
-            value={lastName}
-            onChange={(event) => setLastName(event.target.value)}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                setFirstName('');
-                setLastName('');
-                createGuest().catch((error) => {
-                  console.log(error);
-                });
-              }
-            }}
+            name="firstname"
+            placeholder="type first name"
+            value={firstName}
+            onChange={(event) => setFirstName(event.target.value)}
           />
         </div>
+      </form>
 
-        <div className="guests">
-          <h2>See who is coming:</h2>
-          {guests.map((guest) => (
-            <div key={`user-${guest.id}`} data-test-id="guest">
-              <div>
-                First Name: {guest.firstName}
-                <br />
-                Last Name: {guest.lastName}
-                <br />
-                Coming: {JSON.stringify(guest.attending)}
-                <br />
-                <label key={`guest-${guest.id}`}>
-                  Attending:
-                  <input
-                    type="checkbox"
-                    checked={guest.attending}
-                    aria-label={`${guest.firstName} ${guest.lastName} attending status`}
-                    onChange={() => isAttending(guest.id, guest.attending)}
-                  />
-                </label>
-                <br />
-              </div>
-              <button
-                type="button"
-                aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
-                onClick={() => {
-                  removeGuest(guest.id).catch((error) => {
-                    console.log(error);
-                  });
-                }}
-              >
-                Delete
-              </button>
-              <hr />
-            </div>
-          ))}
-        </div>
+      <div>
+        Last name
+        <input
+          name="lastname"
+          placeholder="type last name"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              setFirstName('');
+              setLastName('');
+              createGuest().catch((error) => {
+                console.log(error);
+              });
+            }
+          }}
+        />
       </div>
-    );
-  }
+
+      <div className="guests">
+        <h2>See who is coming:</h2>
+        {guests.map((guest) => (
+          <div key={`user-${guest.id}`} data-test-id="guest">
+            <div>
+              First Name: {guest.firstName}
+              <br />
+              Last Name: {guest.lastName}
+              <br />
+              Coming: {JSON.stringify(guest.attending)}
+              <br />
+              <label key={`guest-${guest.id}`}>
+                Attending:
+                <input
+                  type="checkbox"
+                  checked={guest.attending}
+                  aria-label={`${guest.firstName} ${guest.lastName} attending status`}
+                  onChange={() => isAttending(guest.id, guest.attending)}
+                />
+              </label>
+              <br />
+            </div>
+            <button
+              type="button"
+              aria-label={`Remove ${guest.firstName} ${guest.lastName}`}
+              onClick={() => {
+                removeGuest(guest.id).catch((error) => {
+                  console.log(error);
+                });
+              }}
+            >
+              Delete
+            </button>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
